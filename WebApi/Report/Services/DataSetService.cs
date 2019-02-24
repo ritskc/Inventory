3115643fbad6aab3ba1f9b9d3bdb6e9571857617
Ritesh Charkhawala <ritesh_charkhawala@intuit.com>
2019-02-24 16:23:37 -0500

    Report Project Added

---
+++ WebApi/Report/Services/DataSetService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAL.Models;
+using DAL.IRepository;
+using DAL.Repository;
+using DAL.DBHelper;
+
+namespace Report.Services
+{
+    public class DataSetService
+    {        
+        public static List<Company> GetCompanies(string contactname)
+        {
+            ISqlHelper sqlHelper = new SqlHelper();
+            ICompanyRepository companyRepository = new CompanyRepository(sqlHelper);
+           
+            return companyRepository.GetAllCompany().ToList();
+                     
+        }
+    }
+}
