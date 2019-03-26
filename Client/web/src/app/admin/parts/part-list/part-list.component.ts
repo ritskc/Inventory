0f01ec0567fd977c178c5d690f6c3628aae4ee5e
Sheshadrinath R <sheshadrinathr@Sheshadrinaths-MacBook-Pro.local>
2019-03-25 22:41:04 -0400

    Parts list and redirection completed

--- Client/web/src/app/admin/parts/part-list/part-list.component.ts
+++ Client/web/src/app/admin/parts/part-list/part-list.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Part } from '../../../models/part.model';
+import { PartsService } from '../parts.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { httpLoaderService } from '../../../common/services/httpLoader.service';
+import { CompanyService } from '../../../company/company.service';
+import { DataColumn } from '../../../models/dataColumn.model';
+import { UserAction } from '../../../models/enum/userAction';
 
 @Component({
   selector: 'app-part-list',
@@ -7,9 +14,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartListComponent implements OnInit {
 
-  constructor() { }
+  parts: Part[] = [];
+  columns: DataColumn[] = [];
+  currentlyLoggedInCompanyId: number = 0;
+
+  constructor(private service: PartsService, private activatedRoute: ActivatedRoute, private router: Router,
+              private httpLoader: httpLoaderService, private companyService: CompanyService) { }
 
   ngOnInit() {
+    this.currentlyLoggedInCompanyId = this.companyService.getCurrentlyLoggedInCompanyId();
+    this.initializeGridColumns();
+    this.getAllPartsForCompany();
   }
 
+  initializeGridColumns() {
+    this.columns.push( new DataColumn({ headerText: "Code", value: "code", isLink: true, sortable: true }) );
+    this.columns.push( new DataColumn({ headerText: "Description", value: "description", sortable: true }) );
+    this.columns.push( new DataColumn({ headerText: "Weight (Kgs)", value: "weightInKg", sortable: true }) );
+    this.columns.push( new DataColumn({ headerText: "Weight (Lbs)", value: "weightInLb", sortable: true }) );
+    this.columns.push( new DataColumn({ headerText: "Min Qty", value: "minQty", sortable: true }) );
+    this.columns.push( new DataColumn({ headerText: "Max Qty (Lbs)", value: "maxQty", sortable: true }) );
+    this.columns.push( new DataColumn({ headerText: "Active", value: "isActive", sortable: true }) );
+    this.columns.push( new DataColumn({ headerText: "Sample", value: "isSample", sortable: true }) );
+  }
+
+  getAllPartsForCompany() {
+    this.httpLoader.show();
+    this.service.getAllParts(this.currentlyLoggedInCompanyId)
+        .subscribe((parts) => {
+          this.parts = parts;
+          this.httpLoader.hide();
+        },
+        (error) => {
+          console.log(error);
+          this.httpLoader.hide();
+        }
+      );
+  }
+
+  rowSelected(row) {
+    this.router.navigateByUrl(`/parts/detail/${ UserAction.Edit }/${row.id}`);
+  }
+
+  addPart() {
+    this.router.navigateByUrl(`/parts/detail/${ UserAction.Add }/#`);
+  }
 }
--- Client/web/src/app/models/part.model.ts
+++ Client/web/src/app/models/part.model.ts
@@ -1,6 +1,52 @@
+
 export class Part {
+
+    constructor() {
+        this.partSupplierAssignments = [];
+        this.partCustomerAssignments = [];
+    }
+
     id: number;
-    name: string;
+    code: string;
     description: string;
+    companyId: number;
+    weightInKg: number;
+    weightInLb: number;
+    openingQty: number;
+    minQty: number;
+    maxQty: number;
+    drawingNo: number;
+    drawingUploaded: boolean;
+    drawingFileName: string;
+    location: string;
+    isActive: boolean;
     isSample: boolean;
+    
+    partSupplierAssignments: PartSupplierAssignment[];
+    partCustomerAssignments: PartCustomerAssignment[];
+}
+
+export class PartSupplierAssignment
+{
+    id: number;
+    supplierID: number;
+    mapCode: string;
+    description: string;
+    qtyInHand: number;
+    qtyInTransit: number;
+    totalQty: number;
+    unitPrice: number;
+}
+
+export class PartCustomerAssignment
+{
+    id: number;
+    customerId: number;
+    mapCode: string;
+    description: string;
+    weight: number;
+    rate: number;
+    surchargePerPound: number;
+    openingQty: number;
+    surchargeExist: boolean;
 }
